3b9e29cf9183df900503b801c8919ebd48e7f7a2
codysilverglat <c.silverglat@gmail.com>
2020-02-16 22:40:22 -0600

    Creates App.js.

--- date-picker/src/App.js
+++ date-picker/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MyDatePicker from './MyDatePicker/MyDatePicker';
+
+function onChange(timestamp) {
+  console.log("Timestamp: ", timestamp);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyDatePicker onChange={onChange}/>MyDatePicker
     </div>
   );
 }
