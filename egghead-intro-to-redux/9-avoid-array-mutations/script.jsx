281f7ebae8b81c2c898edf5e19e8c06aaba99dab
codysilverglat <c.silverglat@gmail.com>
2020-04-19 23:40:43 -0500

    Complete lesson 9

---
+++ egghead-intro-to-redux/9-avoid-array-mutations/script.jsx
@@ -0,0 +1,66 @@
+const addCounter = (list) => {
+  // return list.concat(0);
+    // or in ES6 with spread operator:
+  return [...list, 0];
+};
+
+const removeCounter = (list, index) => {
+  // return list 
+    // .slice(0, index)
+    // .concat(list.slice(index + 1));
+      // or in ES6 with spread operator
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index + 1)
+  ];
+};
+
+const incrementCounter = (list, index) => {
+  // return list
+    // .slice(0, index)
+    // .concat([list[index] + 1])
+    // .concat(list.slice(index + 1));
+      // or in ES6 with spread operator
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+  ];
+};
+
+const testAddCounter = () => {
+  const listBefore = [];
+  const listAfter = [0];
+
+  deepFreeze(listBefore);
+
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter);
+};
+
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+
+  deepFreeze(listBefore);
+
+  expect(
+    removeCounter(listBefore, 1)
+  ).toEqual(listAfter);
+};
+
+const testIncrementCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+
+  deepFreeze(listBefore);
+
+  expect(
+    incrementCounter(listBefore, 1)
+  ).toEqual(listAfter);
+};
+
+testAddCounter();
+testRemoveCounter();
+console.log('All tests passed.');
