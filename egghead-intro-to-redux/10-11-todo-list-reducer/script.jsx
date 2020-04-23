fc33ddd5cb8013d67b85392af44ef0d15ffd0ad8
codysilverglat <c.silverglat@gmail.com>
2020-04-22 23:20:58 -0500

    Complete lessons 10-11

---
+++ egghead-intro-to-redux/10-11-todo-list-reducer/script.jsx
@@ -0,0 +1,30 @@
+const toggleTodo = (todo) => {
+  return {
+    ...todo,
+    completed: !todo.completed
+  };
+};
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
+    
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  };
+  
+  deepFreeze(todoBefore);
+  
+  expect(
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter);
+};
+
+testToggleTodo();
+
+console.log('All tests passed.');
