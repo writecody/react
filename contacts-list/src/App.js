9230d82fb6498dcd173031988b58df80ea22eaec
codysilverglat <c.silverglat@gmail.com>
2020-06-01 15:14:27 -0500

    update grid

--- contacts-list/src/App.js
+++ contacts-list/src/App.js
@@ -3,46 +3,48 @@ import Card from '../src/Card/Card';
 import '../src/App.css'
 
 class App extends Component {
-
+  
   state = {
     contacts: [],
     quotes: []
   }
-
+  
   componentDidMount() {
     fetch('http://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then((data) => {
-        this.setState({ contacts: data })
-      })
-      .catch(console.log);
-
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ contacts: data })
+    })
+    .catch(console.log);
+    
     fetch('https://api.quotable.io/random')
-      .then(response => response.json())
-      .then(quotesData => {
-        this.setState({ quotes: quotesData })
-      }
-      )
-      .then(console.log(this.state.quotes));
+    .then(response => response.json())
+    .then(quotesData => {
+      this.setState({ quotes: quotesData })
+    }
+    )
+    .then(console.log(this.state.quotes));
   }
-
+  
   render () {
     return (
       <>
-        <div className="app-container">
-          <div className="header">
-            <h1 className="list-title">Contact List</h1>
-          </div>
-          <div className="main">
-            <Card 
-              contacts={this.state.contacts}
-              quotes={this.state.quotes}>
-            </Card>
-          </div>
-        </div>
+      <div className="app-container">
+      <div className="header">
+      <h1 className="list-title">Contact List</h1>
+      </div>
+      <div className="contacts">
+        <Card 
+        contacts={this.state.contacts}
+        quotes={this.state.quotes}>
+        </Card>
+      </div>
+      <div className="main">
+      </div>
+      </div>
       </>
-    );
+      );
+    }
   }
-}
-
-export default App;
+  
+  export default App;
